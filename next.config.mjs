f670c9e75de03d948c7abb6cc05829ad9a53e9ed
Netekflix <netekflix@example.com>
2026-02-03 19:39:45 +0000

    fix: allow amazon image domains

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images:{
+    images: {
         remotePatterns: [
             {
                 protocol: 'https',
                 hostname: "image.tmdb.org",
+                port: "",
+            },
+            {
+                protocol: 'https',
+                hostname: "m.media-amazon.com",
+                port: "",
+            },
+            {
+                protocol: 'https',
+                hostname: "images-na.ssl-images-amazon.com",
                 port: "",
             }
         ]
